4d0b7a8803a579542d94376a6aa3ccda7e668be6
Viktor Bellavin <viktor_bellavin@epam.com>
2020-04-22 16:08:09 +0300

    git-version: supported GITHUB_REF env var (Gihub Actions)

--- git-version.gradle.kts
+++ git-version.gradle.kts
@@ -1,16 +1,22 @@
-  
 import java.io.*
 
-if (version == Project.DEFAULT_VERSION && rootProject.file(".git").exists()) {
-    val tagPattern = "v[0-9]*.[0-9]*.[0-9]*"
+if (version == Project.DEFAULT_VERSION) {
+    version = fromEnv() ?: fromGit() ?: version
+}
+
+fun fromEnv(): String? = System.getenv("GITHUB_REF")?.let {
+    "refs/tags/v(.*)".toRegex().matchEntire(it)
+}?.groupValues?.get(1)
+
+fun Project.fromGit(): String? = takeIf { rootDir.resolve(".git").isDirectory }?.run {
+    val output = ByteArrayOutputStream()
     val abbrevRegex = "-(\\d+)-g([0-9a-f]+)$".toRegex()
-    version = ByteArrayOutputStream().let { output ->
-        exec {
-            commandLine("git", "describe", "--tags", "--long", "--match", tagPattern)
-            standardOutput = output
-            isIgnoreExitValue = true
-        }.takeIf { it.exitValue == 0 }
-            ?.let { "$output".substring(1).trim() }
-            ?.replace(abbrevRegex, "")
-    } ?: version
+    exec {
+        val tagPattern = "v[0-9]*.[0-9]*.[0-9]*"
+        commandLine("git", "describe", "--tags", "--long", "--match", tagPattern)
+        standardOutput = output
+        isIgnoreExitValue = true
+    }.takeIf { it.exitValue == 0 }
+        ?.let { "$output".substring(1).trim() }
+        ?.replace(abbrevRegex, "")
 }
