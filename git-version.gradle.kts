88b3eea5da90cd525590e9d8ea410e41645bde11
Viktor Bellavin <viktor_bellavin@epam.com>
2020-12-22 17:28:30 +0300

    EPMDJ-5768 Fixed getting version from git - handled empty "git describe" output.

--- git-version.gradle.kts
+++ git-version.gradle.kts
@@ -1,5 +1,3 @@
-import java.io.*
-
 if (version == Project.DEFAULT_VERSION) {
     version = fromEnv() ?: fromGit() ?: version
 }
@@ -10,13 +8,16 @@ fun fromEnv(): String? = System.getenv("GITHUB_REF")?.let {
 
 fun Project.fromGit(): String? = takeIf { rootDir.resolve(".git").isDirectory }?.run {
     val output = ByteArrayOutputStream()
-    val abbrevRegex = "-(\\d+)-g([0-9a-f]+)$".toRegex()
     exec {
         val tagPattern = "v[0-9]*.[0-9]*.[0-9]*"
         commandLine("git", "describe", "--tags", "--long", "--match", tagPattern)
         standardOutput = output
         isIgnoreExitValue = true
-    }.takeIf { it.exitValue == 0 }
-        ?.let { "$output".substring(1).trim() }
-        ?.replace(abbrevRegex, "")
+    }.run { it.exitValue == 0 }
+        ?.let {
+            val abbrevRegex = "-(\\d+)-g([0-9a-f]+)$".toRegex()
+            "$output".run {
+                trim().removePrefix("v").replace(abbrevRegex, "")
+            }.takeIf(String::any)
+        }
 }
